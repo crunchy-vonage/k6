0f3fc1ccb3e8f35f54e999c8d57bd01914204844
matthew churcher <64464393+crunchy-vonage@users.noreply.github.com>
2021-10-19 22:46:43 +0100

    Update remote.js

--- remote.js
+++ remote.js
@@ -3,13 +3,13 @@ import { sleep, check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '15m', target: 1000 },
+    { duration: '10m', target: 1000 },
   ],
  };
 
 export default function () {
 
-  const res = http.get('http://hello.churcher.me');
+  const res = http.get('http://contact.churcher.me');
 
   check(res, {
     'is status 200': (r) => r.status === 200,
